0f5cb95099c28ec509b6eab6d6b4f7fa85d87c96
Eric Nielsen <4120606+ericbn@users.noreply.github.com>
2024-06-17 20:25:20 -0500

    Show warning on additional compinit calls
    It even shows the script where the additional call is happening when
    it's after the completion module. Doing the same for a call before the
    completion module would be more tricky and involve adding some code in
    ~/.zshenv (or more ideally /etc/zshenv) for the debugging purpose.
    
    Closes #21

--- init.zsh
+++ init.zsh
@@ -6,6 +6,10 @@ if [[ ${TERM} == dumb ]]; then
   return 1
 fi
 
+if (( ${+_comps} )); then
+  print -u2 'warning: completion was already initialized before completion module. Will call compinit again. See https://github.com/zimfw/zimfw/wiki/Troubleshooting#completion-is-not-working'
+fi
+
 () {
   builtin emulate -L zsh -o EXTENDED_GLOB
 
@@ -37,6 +41,10 @@ fi
   if [[ ! ${zdumpfile}.zwc -nt ${zdumpfile} ]] zcompile ${zdumpfile}
 }
 
+functions[compinit]=$'print -u2 \'warning: compinit being called again after completion module at \'${funcfiletrace[1]}
+'${functions[compinit]}
+
+#
 #
 # Zsh options
 #
